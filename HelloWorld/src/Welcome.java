9bb3a43e683c46a58aaeb046c96f87f862957913
Ramu <Ramu@172.17.30.69>
2019-01-04 10:35:20 -0500

    Changes made at Line Number 7 at printing statement

---
+++ HelloWorld/src/Welcome.java
@@ -0,0 +1,12 @@
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("Learn JAVA Completely");
+		System.out.println("ALL THE BEST");
+
+	}
+
+}
