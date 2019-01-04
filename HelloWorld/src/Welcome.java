e265c0429ab9564643d856a1934cbb31ea8dda06
Ramu <Ramu@172.17.30.69>
2019-01-04 10:39:02 -0500

    changes made again in printing statement

--- HelloWorld/src/Welcome.java
+++ HelloWorld/src/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("Learn JAVA Completely");
+		System.out.println("Learn JAVA Completely on Oracle CES");
 		System.out.println("ALL THE BEST");
 
 	}
